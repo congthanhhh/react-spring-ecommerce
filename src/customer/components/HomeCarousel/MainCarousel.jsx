166d2b65e77190c63877d96986afe37003cf37e2
congthanhhh <luffymonkeydct@gmail.com>
2024-02-29 23:03:22 +0700

    frist commit

---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { mainCarouselData } from './MainCarouselData';
+
+const items = mainCarouselData.map((item) => {
+    return <img className="cursor-pointer" role="presentation" src={item.image} alt="" />;
+});
+
+const MainCarousel = () => (
+    <AliceCarousel items={items} disableButtonsControls autoPlay autoPlayInterval={1000} infinite />
+);
+
+export default MainCarousel;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainCarousel from '../../components/HomeCarousel/MainCarousel';
+import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel';
+
+const HomePage = () => {
+    return (
+        <div>
+            <MainCarousel />
+            <div className="">
+                <HomeSectionCarousel />
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
